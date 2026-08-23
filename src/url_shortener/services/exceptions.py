d12cf926c7fa81d5f3808e8a404a52363c9dbfc7
mohammadsoltani1 <mohammadsoltani1br@gmail.com>
2026-08-23 09:01:03 +0330

    add exceptions for service layer

---
+++ src/url_shortener/services/exceptions.py
@@ -0,0 +1,6 @@
+class invalid_url(Exception):
+    """Raised when the provided URL is invalid."""
+    pass
+class short_code_generation_failed(Exception):
+    """Raised when the short code generation fails."""
+    pass
